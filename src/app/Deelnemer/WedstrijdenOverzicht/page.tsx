f7c146e66141e6560f8e96396a07362d40ce0100
Kenneth Haspeel <github@haspeel.be>
2025-02-13 15:27:31 +0100

    wedstrijden overzicht mobiele versie

--- src/app/Deelnemer/WedstrijdenOverzicht/page.tsx
+++ src/app/Deelnemer/WedstrijdenOverzicht/page.tsx
@@ -1,6 +1,6 @@
 import GeenToegang from "@/components/GeenToegang";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import React from "react";
+import React, { Fragment } from "react";
 import { GetWedstrijden } from "../../../../prisma/queries/WedstrijdenQueries";
 import {
   Table,
@@ -15,7 +15,7 @@ import { DateToDDMMYYYY, DatumVoorbij } from "@/components/DatumFuncties";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 const page = async () => {
-  const {  isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated } = getKindeServerSession();
   const auth = await isAuthenticated();
   if (!auth) {
     return <GeenToegang />;
@@ -27,7 +27,7 @@ const page = async () => {
       <div className="flex flex-col w-full">
         <h2 className="text-2xl m-4">Overzicht Wedstrijden</h2>
         <div className="w-full m-4">
-          <Table className="bg-black/20 dark:bg-white/80 dark:text-black w-full lg:w-3/4">
+          <Table className="bg-black/20 dark:bg-white/80 dark:text-black w-full hidden md:block lg:w-3/4">
             <TableHeader>
               <TableRow>
                 <TableHead className="w-1/4 text-black text-lg">
@@ -59,17 +59,72 @@ const page = async () => {
                           </Link>
                         </Button>
                       ) : (
-                        <Button disabled variant="outline" className="dark:text-white w-full">
+                        <Button
+                          disabled
+                          variant="outline"
+                          className="dark:text-white w-full"
+                        >
                           Nog niet verwerkt
                         </Button>
                       )
                     ) : (
-                      <Button disabled variant="outline" className="dark:text-white w-full">
+                      <Button
+                        disabled
+                        variant="outline"
+                        className="dark:text-white w-full"
+                      >
                         Nog niet gereden
                       </Button>
                     )}
                   </TableCell>
                 </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="m-4 md:hidden">
+          <Table className="me-2 pe-2">
+            <TableBody>
+              {lijst?.map((wedstrijd) => (
+                <Fragment key={`fragment_${wedstrijd.id}`}>
+                  <TableRow key={`wedstrijd_${wedstrijd.id}`}>
+                    <TableCell key={`datum_${wedstrijd.id}`}>{DateToDDMMYYYY(wedstrijd.datum)}</TableCell>
+                    <TableCell key={`benaming_${wedstrijd.id}`}>{wedstrijd.naam}</TableCell>
+                  </TableRow>
+                  <TableRow key={`status_${wedstrijd.id}`}>
+                    <TableCell colSpan={2} className="border-b-gray-800 border-b-2">
+                      {DatumVoorbij(wedstrijd.datum) ? (
+                        wedstrijd.afgesloten ? (
+                          <Button className="bg-green-800 text-white" asChild>
+                            <Link
+                              href={`/Deelnemer/WedstrijdUitslag/${wedstrijd.id}`}
+                              className="flex justify-center items-center gap-2 ml-0 bg-green-700 w-full"
+                              title="Uitslag Verwerken"
+                            >
+                              Uitslag
+                            </Link>
+                          </Button>
+                        ) : (
+                          <Button
+                            disabled
+                            variant="outline"
+                            className="dark:text-white w-full"
+                          >
+                            Nog niet verwerkt
+                          </Button>
+                        )
+                      ) : (
+                        <Button
+                          disabled
+                          variant="outline"
+                          className="dark:text-white w-full"
+                        >
+                          Nog niet gereden
+                        </Button>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                </Fragment>
               ))}
             </TableBody>
           </Table>
