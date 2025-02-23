4fae329b7d19f36a1eda86f39f640c7f51778e75
Kenneth Haspeel <github@haspeel.be>
2025-02-23 14:40:57 +0100

    start quill editor

--- src/app/Admin/ZendMail/page.tsx
+++ src/app/Admin/ZendMail/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import QuillPage from "./QuillPage"
 
 const QuillServer = () => {
   return (
     <>
     <p>hier komt een text editor</p>
+    <QuillPage/>
     </>
   )
 }
